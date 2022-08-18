4e517268f65f02540e1848ab8414cfbff17a0740
vitor lopes <lopesvitor959@gmail.com>
2022-08-18 14:37:51 +0100

    Render Movies List

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,14 +1,42 @@
 import React from 'react';
 import { useStateContext } from '../context/StateContextProvider';
+import {Link} from 'react-router-dom'
 
+const backUpPoster =  "https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png"
 
 const MoviesList = () => {
 
- const {dummyValue} = useStateContext();
- console.log(dummyValue);
+ const { moviesData, isLoading} = useStateContext();
+
+ if(isLoading) {
+    return (
+        <div className='loading'></div>
+    )
+}
 
   return (
-    <div>MoviesList</div>
+    <section className='movies'>
+        {moviesData.map( movie => {
+            const {Poster, Title, Year, imdbID} = movie;
+            return(
+                <div className="movie">
+                    {
+                        <Link to= {`/movies/${imdbID}` }>
+                            <article>
+                                <img src={Poster === "N/A" ? backUpPoster : Poster} alt={Title} />
+                                <div className="movie-info">
+                                    <h4>{Title}</h4>
+                                    <p>{Year}</p>
+                                </div>
+                            </article>
+                       </Link>
+                    }
+                </div>
+            )
+        }
+            
+        )}
+    </section>
   )
 }
 
--- src/context/StateContextProvider.js
+++ src/context/StateContextProvider.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useEffect } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 
 const StateContext = createContext();
 // http://www.omdbapi.com/?apikey=[yourkey]&
@@ -8,17 +8,22 @@ const StateContext = createContext();
 
 export const StateContextProvider = ( {children} ) => { 
 
+    const [moviesData, setMoviesData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true)
+
     const dummyValue = "This is a dummy value";
 
     const fetchData = async () => {
+        setIsLoading(true)
         try {
             const res = await fetch("https://www.omdbapi.com/?apikey=abfefb4f&s=matrix")
             const data = await res.json();
-            console.log(data)
+            console.log(data.Search)
+            setMoviesData(data.Search)
+            setIsLoading(false)
         } catch (error) {
             console.log(error);
         }
-       
     }
 
     useEffect(() => {
@@ -29,7 +34,9 @@ export const StateContextProvider = ( {children} ) => {
     return(
         <StateContext.Provider
             value={{
-                dummyValue
+                dummyValue,
+                isLoading,
+                moviesData
             }}
         >
             { children }
